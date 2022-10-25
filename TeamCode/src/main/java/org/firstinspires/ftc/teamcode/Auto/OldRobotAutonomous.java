e44868933d7ef6919c27f3b5dbd3d51d48130105
Broyojo <dha@xoba.com>
2022-10-25 11:36:52 -0400

    Added Old Robot Autonomous Skeleton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/OldRobotAutonomous.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Old Robot Autonomous")
+public class OldRobotAutonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        
+    }
+}
