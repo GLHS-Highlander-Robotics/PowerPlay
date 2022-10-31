d0ccc3726d9016e2b0e6966fd8a53334a11b781f
largebread72 <ayushkayal2006@gmail.com>
2022-10-31 11:29:41 -0400

    coords

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/SleeveDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/SleeveDetection.java
@@ -23,11 +23,11 @@ public class SleeveDetection extends OpenCvPipeline {
     }
 
     // TOPLEFT anchor point for the bounding box
-    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(50, 125);
 
     // Width and height for the bounding box
-    public static int REGION_WIDTH = 30;
-    public static int REGION_HEIGHT = 50;
+    public static int REGION_WIDTH = 50;
+    public static int REGION_HEIGHT = 30;
 
     // Lower and upper boundaries for colors
     private static final Scalar
