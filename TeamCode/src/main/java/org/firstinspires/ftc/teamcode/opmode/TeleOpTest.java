4cfa343549b74be882cea5cdd9390b73e06e536c
Broyojo <dha@xoba.com>
2023-02-09 22:39:03 -0500

    added one player teleop test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/TeleOpTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp(name = "TeleOp Test")
+public class TeleOpTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        double limiter = 0.75;
+
+        while (opModeIsActive()) {
+            double forward = gamepad1.left_stick_y * limiter;
+            double strafe = gamepad1.left_stick_x * limiter;
+            double rotate = gamepad1.right_stick_x * limiter;
+
+            if (Math.abs(gamepad1.left_stick_y) < 0.01) {
+                forward = 0;
+            }
+            if (Math.abs(gamepad1.left_stick_x) < 0.01) {
+                strafe = 0;
+            }
+            if (Math.abs(gamepad1.right_stick_x) < 0.01) {
+                rotate = 0;
+            }
+
+            drive.setMotorPowers(
+                    forward + strafe + rotate,
+                    forward - strafe + rotate,
+                    forward - strafe - rotate,
+                    forward + strafe - rotate
+            );
+        }
+    }
+}
