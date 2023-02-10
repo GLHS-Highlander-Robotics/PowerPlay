f4a0e11e96dd914e846781557b6b7af9e73df738
Broyojo <dha@xoba.com>
2023-02-09 23:22:38 -0500

    added basic one-cone auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/AutoTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.slide.LinearSlide;
+
+@Autonomous(name = "Auto Test")
+public class AutoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        LinearSlide slide = new LinearSlide(this);
+
+        slide.release();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
+                .addDisplacementMarker(slide::grab)
+                .forward(5.5)
+                .strafeLeft(37.5)
+                .addDisplacementMarker(() -> slide.setSlideAndWait(1900))
+                .forward(6.5)
+                .addDisplacementMarker(slide::release)
+                .build();
+
+        drive.followTrajectory(trajectory);
+    }
+}
