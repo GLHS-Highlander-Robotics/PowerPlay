5e2bd73548d6a9504c1360939bbc95330fe80246
largebread72 <ayushkayal2006@gmail.com>
2023-02-27 19:44:59 -0500

    First cone complete (~90% acc)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoSkeleton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoSkeleton.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -38,7 +39,7 @@ public class AutoSkeleton extends LinearOpMode {
                 .addDisplacementMarker(slide::grab)
                 .addDisplacementMarker(() -> slide.setSlide(60))
                 .splineToLinearHeading(new Pose2d(10, -22, Math.toRadians(55)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(58, 21, Math.toRadians(55)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(58, -21, Math.toRadians(55)), Math.toRadians(0))
                 .addDisplacementMarker(() -> slide.setSlide(LinearSlide.MAX_HEIGHT))
                 .forward(4.5)
                 .waitSeconds(1.5)
@@ -56,19 +57,46 @@ public class AutoSkeleton extends LinearOpMode {
                 .addDisplacementMarker(slide::grab)
                 .splineToLinearHeading(new Pose2d(0, 0, Math.toRadians(0)), Math.toRadians(0))
                 .addDisplacementMarker(() -> slide.setSlide(LinearSlide.MAX_HEIGHT))
-                .forward(4.5)
+                .forward(5)
                 .waitSeconds(1.5)
                 .addDisplacementMarker(slide::ungrab)
                 .waitSeconds(1.5)
-                .back(4.5)
+                .back(5)
                 .build();
 
+        TrajectorySequence trajA = drive.trajectorySequenceBuilder(new Pose2d(-31.08, -64.54, Math.toRadians(90.00)))
+                .addDisplacementMarker(slide::grab)
+                .splineToConstantHeading(new Vector2d(-12.33, -52.49), Math.toRadians(70.00))
+                .splineToConstantHeading(new Vector2d(-12.53, -31.84), Math.toRadians(90.00))
+                .addDisplacementMarker(() -> slide.setSlide(LinearSlide.MAX_HEIGHT))
+                .splineToConstantHeading(new Vector2d(-24.38, -6.79), Math.toRadians(180.00))
+                .waitSeconds(0.4)
+                .addDisplacementMarker(slide::ungrab)
+                .addDisplacementMarker(() -> slide.setSlide(LinearSlide.MIN_HEIGHT))
+
+                .splineTo(new Vector2d(-24.38, -14.63), Math.toRadians(90.00))
+                .splineToConstantHeading(new Vector2d(-45.61, -12.72), Math.toRadians(180.00))
+                .lineToSplineHeading(new Pose2d(-63.01, -12.33, Math.toRadians(180.00)))
+
+                .addDisplacementMarker(slide::grab)
+                .build();
+        drive.setPoseEstimate(trajA.start());
+//        TrajectorySequence trajB = drive.trajectorySequenceBuilder(new Pose2d(-24.38, -6.79, Math.toRadians(90.00)))
+//                .addDisplacementMarker(() -> slide.setSlide(LinearSlide.MIN_HEIGHT))
+//                .splineToSplineHeading(new Pose2d(-24.00, -13.29, Math.toRadians(180.00)), Math.toRadians(90.00))
+//                .splineToConstantHeading(new Vector2d(-63.97, -12.14), Math.toRadians(180.00))
+//                .addDisplacementMarker(slide::grab)
+//                .build();
+
+
         slide.ungrab();
+        drive.imu.resetYaw();
         waitForStart();
 
         if (isStopRequested()) return;
 
-        drive.followTrajectorySequence(traj1);
+        drive.followTrajectorySequence(trajA);
+//        drive.followTrajectorySequence(trajB);
 
     }
 }
